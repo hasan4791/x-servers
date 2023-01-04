0f6d0f4c4e868e4de96ec3e933a02d51799ef622
hasan4791 <chandrahasan4791@gmail.com>
2023-01-04 21:47:19 +0530

    wireguard changes

---
+++ wireguard/start.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -e
+
+WORKING_DIR="$(pwd)"
+
+if [[ ! -d "${WORKING_DIR}"/config ]]; then
+	mkdir -p "${WORKING_DIR}"/config
+fi
+
+podman run -d \
+	--name=wireguard \
+	--cap-add=NET_ADMIN \
+	-e PUID=1000 \
+	-e PGID=1000 \
+	-e TZ=Asia/Kolkata \
+	-e SERVERURL=wg.tkch.co.in \
+	-e SERVERPORT=51820 \
+	-e PEERS="iphone14plus" \
+	-e PEERDNS=94.140.14.14,94.140.15.15 \
+	-e INTERNAL_SUBNET=172.32.1.0 \
+	-e ALLOWEDIPS=0.0.0.0/0 \
+	-e LOG_CONFS=true \
+	-p 51820:51820/udp \
+	-v "${WORKING_DIR}"/config:/config:Z \
+	--restart always \
+    localhost/xs-wireguard:latest
