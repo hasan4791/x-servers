07b6ebde1cd277e6e7e07be9b3077ff0249b5bf4
hasan4791 <chandrahasan4791@gmail.com>
2023-01-04 20:52:57 +0530

    baseimage & openvpn changes

---
+++ openvpn-as/start.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -e
+
+WORKING_DIR="$(pwd)"
+
+if [[ ! -d "${WORKING_DIR}"/config ]]; then
+	mkdir -p "${WORKING_DIR}"/config
+fi
+
+#Run as Roolful mode
+podman run -d \
+	--name=openvpn-as \
+	--cap-add=NET_ADMIN \
+	--device=/dev/net/tun \
+	-e PUID=1000 \
+	-e PGID=1000 \
+	-h openvpnas \
+	-e TZ=Asia/Kolkata \
+	-p 9943:943/tcp \
+	-p 1194:443/tcp \
+	-p 1194:1194/udp \
+	-v "${WORKING_DIR}"/config:/config:Z \
+	--restart always \
+	localhost/xs-openvpn-as:latest
