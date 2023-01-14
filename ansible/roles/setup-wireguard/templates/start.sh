e8e561e3fdcc0e0da4ee173bcf238307c1c030a7
T K Chandra Hasan <chandrahasan4791@gmail.com>
2023-01-14 23:18:31 +0530

    Wireguard & Openvpn Server (Root & Rootless Podman support) (#2)
    * Add files
    
    * add tz
    
    * updates
    
    * ssl error handling
    
    * Update format
    
    * Ansible automation
    
    * baseimage & openvpn changes
    
    * wireguard changes
    
    * up wg dockerfile
    
    * Cleanup
    
    * cronjob changes
    
    * Update s6 versions
    
    * Update start script
    
    * generic build script
    
    * update cronjob script
    
    * wireguard cleanups
    
    * cleanups
    
    * Slack notification & Dynamic path updates
    
    * Replace vim with nano
    
    * wg updates
    
    * Ansible automation updates
    
    * Update format
    
    * Rootless Mode support
    
    * Updates
    
    * Root/rootless fixes

---
+++ ansible/roles/setup-wireguard/templates/start.sh
@@ -0,0 +1,94 @@
+#!/usr/bin/env bash
+
+set -e
+
+CONTAINER_NAME="$(basename "$(pwd)")"
+CONTAINER_MODE="$1"
+
+if [ -z "${XSERVER_DATA_PATH}" ]; then
+	CONFIG_PATH="$(pwd)"
+else
+	CONFIG_PATH="${XSERVER_DATA_PATH}"/"${CONTAINER_NAME}"
+fi
+
+if [[ ! -d "${CONFIG_PATH}"/config ]]; then
+	mkdir -p "${CONFIG_PATH}"/config
+fi
+
+#Defaults to rootful mode
+CONTAINER_BIN="sudo podman"
+{% if user_id.stdout is defined %}
+CONTAINER_USER={{ user_id.stdout }}
+{% else %}
+CONTAINER_USER="1000"
+{% endif %}
+{% if group_id.stdout is defined %}
+CONTAINER_GROUP={{ group_id.stdout }}
+{% else %}
+CONTAINER_GROUP="1000"
+{% endif %}
+if [ "${CONTAINER_MODE}" == "rootless" ]; then
+	CONTAINER_BIN="podman"
+	# In rootless mode, container root user
+	# is mapped to host's non-root user
+{% if xserver_container_non_root_uid is defined %}
+	CONTAINER_USER={{ xserver_container_non_root_uid }}
+{% else %}
+	CONTAINER_USER=0
+{% endif %}
+	CONTAINER_GROUP=0
+fi
+
+# Run as Root container in podman
+# with PUID & PGID of non-root user
+# inside the container
+${CONTAINER_BIN} run -d \
+	--name="${CONTAINER_NAME}" \
+	--cap-add=NET_ADMIN \
+	-h "${CONTAINER_NAME}" \
+	-e PUID="${CONTAINER_USER}" \
+	-e PGID="${CONTAINER_GROUP}" \
+{% if wg_timezone is defined %}
+	-e TZ={{ wg_timezone }} \
+{% else %}
+	-e TZ=Asia/Kolkata \
+{% endif %}
+{% if wg_server_url is defined %}
+	-e SERVERURL={{ wg_server_url }} \
+{% else %}
+	-e SERVERURL=auto \
+{% endif %}
+{% if wg_server_port is defined %}
+	-e SERVERPORT={{ wg_server_port }} \
+{% else %}
+	-e SERVERPORT=51820 \
+{% endif %}
+{% if wg_peers is defined %}
+	-e PEERS={{ wg_peers }} \
+{% else %}
+	-e PEERS=1 \
+{% endif %}
+{% if wg_peer_dns is defined %}
+	-e PEERDNS={{ wg_peer_dns }} \
+{% else %}
+	-e PEERDNS=auto \
+{% endif %}
+{% if wg_internal_subnet is defined %}
+	-e INTERNAL_SUBNET={{ wg_internal_subnet|ipaddr('address') }} \
+{% else %}
+	-e INTERNAL_SUBNET=172.32.1.0 \
+{% endif %}
+{% if wg_allowed_ips is defined %}
+	-e ALLOWEDIPS={{ wg_allowed_ips|ipaddr('address') }} \
+{% else %}
+	-e ALLOWEDIPS=0.0.0.0/0 \
+{% endif %}
+{% if wg_log_confs is defined %}
+	-e LOG_CONFS={{ wg_log_confs|bool }} \
+{% else %}
+	-e LOG_CONFS=true \
+{% endif %}
+	-p 51820:51820/udp \
+	-v "${CONFIG_PATH}"/config:/config:Z \
+	--restart always \
+	localhost/xs-wireguard:latest
---
+++ cronjobs/update-x-servers.sh
@@ -0,0 +1,108 @@
+#!/usr/bin/env bash
+
+set -ex
+
+if [ -e /tmp/lock ]; then
+	echo "cronjob locked"
+	exit 1
+fi
+
+touch /tmp/lock
+
+if [ "$EUID" -ne 0 ]; then
+	ALERT_MSG="/tmp/alert-rootless.json"
+	CONTAINER_STATUS_TITLE="Rootless Container Status"
+else
+	ALERT_MSG="/tmp/alert.json"
+	CONTAINER_STATUS_TITLE="Container Status"
+fi
+
+#Set default install path
+if [ -z "${XSERVER_PATH}" ]; then
+	XSERVER_PATH="/root/x-servers"
+fi
+
+if [ "$(podman ps -a --format "{{.Names}}" | wc -l)" -eq 0 ]; then
+	echo "No containers are running"
+	exit 0
+fi
+XSERVERS=$(podman ps -a --format "{{.Names}}")
+XSERVER_IMG="baseimage-ubuntu ${XSERVERS}"
+
+send_slack_notification() {
+	STATUS="$1"
+	if [ "${STATUS}" == "success" ]; then
+		STATUS=":white_check_mark:"
+	else
+		STATUS=":x:"
+	fi
+	cp "${XSERVER_PATH}"/cronjobs/alert.json.template "${ALERT_MSG}"
+	sed -i -e "s/_CONTAINER_STATUS_TITLE_/${CONTAINER_STATUS_TITLE}/g" "${ALERT_MSG}"
+	sed -i -e "s/_HOSTNAME_/${HOSTNAME}/g" "${ALERT_MSG}"
+	sed -i -e "s/_STATUS_ICON_/${STATUS}/g" "${ALERT_MSG}"
+	#shellcheck disable=SC2028
+	CONTAINER_STATUS=$(for i in $(podman ps -a --format "{{.Names}}:{{.Status}}" | sed -e "s/ /_/g"); do echo -n "$i\\n"; done)
+	sed -i -e "s/_CONTAINER_STATUS_/${CONTAINER_STATUS}/g" "${ALERT_MSG}"
+	curl -X POST -H 'Content-type: application/json' --data-binary "@${ALERT_MSG}" "$SLACK_URL"
+	rm -rf "${ALERT_MSG}"
+}
+
+handle_failure() {
+	#Start container with previous image
+	start_containers
+	send_slack_notification "fail"
+	rm -f /tmp/lock
+}
+
+start_containers() {
+	for server in ${XSERVERS}; do
+		cd "${XSERVER_PATH}/$server"
+		./start.sh
+		cd -
+	done
+}
+
+#trap on failure
+trap handle_failure EXIT SIGTERM SIGINT
+
+#Stop running containers
+for server in ${XSERVERS}; do
+	set +e
+	podman stop "$server"
+	podman rm "$server"
+	set -e
+done
+
+#Update node packages
+if [ "$EUID" -eq 0 ]; then
+	dnf update -y
+	dnf upgrade -y
+fi
+
+#Update container images
+cd "${XSERVER_PATH}"
+for server in ${XSERVER_IMG}; do
+	./build.sh "${XSERVER_ARCH}" "$server"
+done
+cd -
+
+#Remove failure handling
+trap - EXIT SIGTERM SIGINT
+
+#Start containers
+start_containers
+
+#Clear podman cache
+podman system prune -a -f
+
+#Send notification to Slack
+send_slack_notification "success"
+
+rm -f /tmp/lock
+
+#Reboot node
+if [ "$EUID" -eq 0 ]; then
+	echo "System will reboot in 60 Seconds" | wall
+	sleep 60
+	reboot
+fi
